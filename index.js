965ee7bf59bc4cdad3794adec5ffd8ea4bf1a3fe
Milind <milind1995.mp@gmail.com>
2023-07-21 22:15:28 +0530

    code modified for Router Usege

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express=require('express')
-const connection=require('./connection');
-const bodyParser = require('body-parser');
-const  createDB=require('./dbCOntroller')
-const checkDB=require('./dbCOntroller')
-const router=express.Router();
-
-
-const app=express();
-app.use(router);
-app.use(bodyParser.json());
-
-app.listen(5000,(err)=>{
-    if(err){
+const express = require('express')
+const connection = require('./connection');
+//const bodyParser = require('body-parser');
+const checkDBTable = require('./dbCOntroller')
+const employeeController = require('./EmployeeController/employee')
+const app = express();
+const cors=require('cors');
+app.use(cors());
+app.use(express.json());
+//app.use(bodyParser.json());
+app.listen(5000, (err) => {
+    if (err) {
         console.log(err);
     }
-    else{
+    else {
         console.log("Serveer Running On PORT 5000");
         connection.connect();
     }
 })
 
-router.post('/',checkDB,createDB)
-
-//router.post('/employee',checkEmployee,)
+app.post('/', checkDBTable);
+app.use("/employee", employeeController);
